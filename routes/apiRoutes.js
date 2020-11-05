e6ddad890d660bd0979a243f45b5456cd55d4c0e
Jordan Morse <jtm6496@gmail.com>
2020-11-05 16:40:39 -0500

    adding api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,31 @@
 const router = require("express").Router()
-const store = require("../db/db.json")
+const notes = require("../db/db.json")
+const fs = require("fs")
 
 
 router.get("/notes", (req, res) => {
-    // this is the GET route where your will you 
-    // will utilize the getNMotes() function
+    res.json(notes);
 })
 
-router.post("/notes/:id", (req, res) => {
-    // this is the POST route where your will you 
-    // will utilize the addNotes() function
+router.post("/notes", (req, res) => {
+    var body = req.body;
+    notes.push(body);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err, data) => {
+        if (err) throw (err)
+        else {res.json(body)}
+    })
 })
 
 router.delete("/notes/:id", (req, res) => {
-    // this is the delete route where you will
-    // utilize the removeNote() function
+    req.params.id
+    //don't use index
+    //add ids to db.json and post route
+    //once you figure out how you add ids, add to post
+    //write a for loop that looks for specfic id
+    //if notes[i].id = req.params.id (.splice())
+    //write file with new notes
+    //res.send(notes)
+    //npm package for ids
 })
 
 module.exports = router
