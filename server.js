3b3a21733c186cee07b326aabbcb2783c8b3238c
Jordan Morse <jtm6496@gmail.com>
2020-11-05 16:01:51 -0500

    adding html/api routes

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var htmlRoutes = require("./routes/htmlRoutes.js");
+var apiRoutes = require("./routes/apiRoutes.js");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
